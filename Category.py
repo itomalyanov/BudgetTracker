1497937ddec8654e5073dd590bd03e1254ef6123
Ilian Tomalyanov <i.tomalyanov@gmail.com>
2023-04-08 14:35:32 +0300

    update categories

--- Category.py
+++ Category.py
@@ -1,7 +1,10 @@
+import json
 class Category:
 
     def __init__(self):
         self.category = None
+        file = open('CategoriesList.json')
+        cat_list = json.load(file)
 
     def set_category(self, category):
         self.category = category
